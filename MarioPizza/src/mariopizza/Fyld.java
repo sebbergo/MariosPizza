68372740194cd601cbf027addccd10bc86bc337e
Marc <marc.g.ekstrom@gmail.com>
2019-10-23 15:03:28 +0200

    new lort

--- MarioPizza/src/mariopizza/Fyld.java
+++ MarioPizza/src/mariopizza/Fyld.java
@@ -1,5 +1,19 @@
 package mariopizza;
 
-class Fyld {
+public class Fyld {
+    String type;
 
+    public Fyld(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    
 }
