ae98449088c4f146ccec665bc9f1201c01d8045a
Marc <marc.g.ekstrom@gmail.com>
2019-10-23 14:45:39 +0200

    lol

--- MarioPizza/src/mariopizza/MarioPizza.java
+++ MarioPizza/src/mariopizza/MarioPizza.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 public class MarioPizza {
     ArrayList <Bestillinger> bestillinger = new ArrayList();
-    
+    ArrayList <Fyld> fyld = new ArrayList();
+    ArrayList <Pizza> pizza = new ArrayList ();
     
 }
