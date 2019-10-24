d97ca66e5964e8aeb8fb940ff9bad471c5cd634f
Marc <marc.g.ekstrom@gmail.com>
2019-10-24 12:40:07 +0200

    Slettet main og indsat den i MarioPizza

--- MarioPizza/src/mariopizza/MarioPizza.java
+++ MarioPizza/src/mariopizza/MarioPizza.java
@@ -3,10 +3,22 @@ package mariopizza;
 import java.util.ArrayList;
 
 public class MarioPizza {
-    ArrayList <Bestilling> bestillinger = new ArrayList();
-    
-    public void addBestilling(Bestilling bestilling){
-        bestillinger.add(bestilling);
-    }
+    public static void main(String[] args) {
+        Pizza pep = new Pizza("pep",100,"Tomat,Pep,ost");
+        Pizza ost = new Pizza("ost",90,"Tomat,ost");
+        Pizza tomat = new Pizza("tomat",80,"Tomat");
         
+        Menukort menukort = new Menukort();
+        
+        menukort.addPizza(pep);
+        menukort.addPizza(ost);
+        menukort.addPizza(tomat);
+        
+        Menukort.printPizza();
+        System.out.println("");
+        
+        Bestilling bes1 = new Bestilling("10:30", "1,2,1,1,1");
+        bes1.printBes();
+
+    }
 }
