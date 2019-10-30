f238c5c2783964dbb64132dd179b8ab56eac73e8
sebbergo <sebastian@godskhansen.dk>
2019-10-30 15:38:14 +0100

    seb comments controller

--- MarioPizza/src/mariopizza/Controller.java
+++ MarioPizza/src/mariopizza/Controller.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Controller {
 
+    //Programmet starter og mulighederne vises på skærmen
     public static void run() {
         Menukort.menukortLoad();
         Statistik.StatLoad();
@@ -14,8 +15,7 @@ public class Controller {
 
         Statistik.bestillingerEfterTid();
         Statistik.gemBestillingerCsv();
-        
-        
+
         while (exitValue) {
             int cases = 1;
             //valg muligheder til brugeren
@@ -33,6 +33,8 @@ public class Controller {
                 case 1:
                     String tid = "";
 
+                    //if else statements der sørger for programmet fortsætter
+                    //uanset om man går videre eller stopper
                     String pizzaNummere = "";
                     String navn = "";
                     int tlfnr = 0;
@@ -67,6 +69,7 @@ public class Controller {
                     break;
 
                 case 2:
+                    //fjern eksisterende bestilling
                     int bestillingId = 0;
                     tempInput = getUserText("Skriv nummer på den bestilling du ønsker at fjerne");
                     if (!tempInput.contains("stop")) {
@@ -75,10 +78,10 @@ public class Controller {
                         break;
                     }
 
-                    // int bestillingId = getUserNumber("Skriv nummer på den bestilling du ønsker at fjerne");
                     fjernBestilling(bestillingId);
                     break;
                 case 3:
+                    //Lav en ny pizza der bliver tilføjet til csv fil
                     String pizzaNavn = "";
                     int pizzaPris = 0;
                     String fyld = "";
@@ -105,7 +108,9 @@ public class Controller {
                     Menukort.addPizzaToCsv(pizzaNavn, pizzaPris, fyld);
                     Menukort.gemCsv();
                     break;
+                    
                 case 4:
+                    //Fjern eksisterende pizza fra csv fil
                     Menukort.printPizza();
                     int pizzaId = 0;
                     tempInput = getUserText("Skriv nummer på pizza du ønsker at slette");
@@ -117,7 +122,9 @@ public class Controller {
 
                     Menukort.sletLine(pizzaId);
                     break;
+                    
                 case 5:
+                    //Antal køb af hver pizza eller alle bestillinger
                     cases = getUserNumber(
                             "Skriv 1 for at se antal køb af hver pizza\n"
                             + "Skriv 2 for at se bestillinger");
@@ -132,9 +139,13 @@ public class Controller {
 
                     }
                     break;
+                
+                //Se menukort
                 case 6:
                     Menukort.printMenukort();
                     break;
+                
+                //Luk program
                 case 7:
                     exitValue = false;
                     System.out.println("Program lukker!");
@@ -145,14 +156,14 @@ public class Controller {
 
     //Methode til at hente alle pizzaer
     public static String getPizzaer(String skriv) {
-        //retVal den værdig vi 'nsker at retuner
+        //retVal den værdig vi ønsker at retuner
         //pizza er den pizza vi lige har hentet og pizzaNummere er alle pizzer der er blevet valgt i en String
         String retVal = "";
         String pizza = "";
         String pizzaNummere = "";
 
-        //While loop der køre indtil brugeren skriver exit
-        //hvergang det køre spøgere det om en pizza
+        //While loop der kører indtil brugeren skriver exit
+        //hvergang det køre spørger det om en pizza
         while (!pizza.equals("exit") && !pizza.equals("stop")) {
             pizza = getUserText(skriv);
             pizzaNummere += pizza + ",";
@@ -160,11 +171,13 @@ public class Controller {
         if (pizza.equals("stop")) {
             return pizzaNummere.substring(0, pizzaNummere.length() - 1);
         }
-        //Fjerner ,exit, fra stringen før vi sender den retur 
+        //Fjerner exit fra stringen før vi sender den retur 
         retVal = pizzaNummere.substring(0, pizzaNummere.length() - 5);
         return retVal;
     }
 
+    //Vi bruger den her som String fra useren istedet for
+    //at bruge scanneren manuelt hver gang
     public static String getUserText(String skriv) {
         Scanner myScan = new Scanner(System.in);
         System.out.println(skriv);
@@ -172,6 +185,8 @@ public class Controller {
         return resultat;
     }
 
+    //Vi bruger den her som int fra useren istedet for
+    //at bruge scanneren manuelt hver gang
     public static int getUserNumber(String skriv) {
         Scanner myScan = new Scanner(System.in);
         System.out.println(skriv);
@@ -180,6 +195,8 @@ public class Controller {
         return resultat;
     }
 
+    //Metode til at tilføje en bestilling og adde den direkte til arrayliste
+    //med bestillinger og statistikken
     public static void tilføjBestilling(String tid, String pizzaNummer, String navn, int tlfnr) {
         Bestilling bes;
         if (tlfnr == 0) {
@@ -191,25 +208,27 @@ public class Controller {
         Statistik.addBestillingStat(bes);
     }
 
+    //Fjern bestilling fra bestillingsliste array, hvis bestillingen findes
     public static void fjernBestilling(int id) {
         Bestilling bes = Statistik.bestillingChecker(id);
         if (bes != null) {
             Statistik.removeBestilling(bes);
         }
     }
-
+    
+    //metode til at sætte tidspunkt bestillingen skal hentes på
     public static String getTid(String time, String min) {
         String satTidTime = "";
         String satTidMin = "";
         int satTidTimeInt = -1;
         int satTidMinInt = -1;
 
-        while(!satTidTime.contains("stop") && satTidTimeInt < 0 || !satTidTime.contains("stop") && satTidTimeInt > 24){
+        while (!satTidTime.contains("stop") && satTidTimeInt < 0 || !satTidTime.contains("stop") && satTidTimeInt > 24) {
             satTidTime = getUserText(time);
             satTidTimeInt = Integer.parseInt(satTidTime);
 
         }
-        while(!satTidMin.contains("stop") && satTidMinInt < 0 || !satTidMin.contains("stop") && satTidMinInt > 60){
+        while (!satTidMin.contains("stop") && satTidMinInt < 0 || !satTidMin.contains("stop") && satTidMinInt > 60) {
             satTidMin = getUserText(min);
             satTidMinInt = Integer.parseInt(satTidMin);
         }
@@ -217,6 +236,5 @@ public class Controller {
         System.out.println(satTidTimeInt);
         return satTidTime + ":" + satTidMin;
     }
-    
-    
+
 }
